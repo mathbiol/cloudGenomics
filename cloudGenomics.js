736c661bc9ac6521bc87ac559e7b2a672ea106a7
Jonas Almeida <jalmeida@mathbiol.org>
2015-10-06 09:58:30 -0400

    resolvido, desculpa o mau geito

--- cloudGenomics.js
+++ cloudGenomics.js
@@ -1,12 +1,7 @@
 console.log('cloudGenomics :-)!')
 //alert(8)
 
-<<<<<<< .merge_file_a07312
-cloudGenomics=function(){}
-
 console.log('hello outravez')
-=======
 cloudGenomics=function(){
     return "Hello!" //
 }
->>>>>>> .merge_file_a03516

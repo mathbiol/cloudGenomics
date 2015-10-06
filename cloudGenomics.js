6d1970e6cfec4c1ecdf36632fb8570c4e4d76440
Jonas Almeida <jalmeida@mathbiol.org>
2015-10-06 10:06:51 -0400

    jQuery added programmatically

--- cloudGenomics.js
+++ cloudGenomics.js
@@ -5,3 +5,10 @@ console.log('hello outravez')
 cloudGenomics=function(){
     return "Hello!" //
 }
+ip = document.createElement('input')
+ip.id="lala"
+document.body.appendChild(ip)
+ip.onkeyup=function(){console.log(ip.value)}
+s=document.createElement('script')
+s.src="https://code.jquery.com/jquery-2.1.4.min.js"
+document.head.appendChild(s)

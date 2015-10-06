ae0832efddaa956925073301a02825ee6764cf25
Jonas Almeida <jalmeida@mathbiol.org>
2015-10-06 10:09:44 -0400

    telling the console about jQuery being there or not

--- cloudGenomics.js
+++ cloudGenomics.js
@@ -11,4 +11,8 @@ document.body.appendChild(ip)
 ip.onkeyup=function(){console.log(ip.value)}
 s=document.createElement('script')
 s.src="https://code.jquery.com/jquery-2.1.4.min.js"
+s.onload=function(){
+    console.log('e agora ja existe:',typeof(jQuery)) // <-- prova que existes
+}
+console.log('jQuery ainda nao existe:',typeof(jQuery))
 document.head.appendChild(s)

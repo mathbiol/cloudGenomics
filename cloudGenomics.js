72545b7e0c15ec1e8f90ec5f2bb77f2269d985b7
dmffrancisco <dmffrancisco91@gmail.com>
2015-10-06 13:32:11 +0100

    whatever

--- cloudGenomics.js
+++ cloudGenomics.js
@@ -1,6 +1,12 @@
 console.log('cloudGenomics :-)!')
 //alert(8)
 
+<<<<<<< .merge_file_a07312
 cloudGenomics=function(){}
 
 console.log('hello outravez')
+=======
+cloudGenomics=function(){
+    return "Hello!" //
+}
+>>>>>>> .merge_file_a03516

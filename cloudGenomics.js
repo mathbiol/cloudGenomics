ab68f2b03e6aaca07ef16d4f2f9b4010779326e4
Jonas Almeida <jalmeida@mathbiol.org>
2015-10-09 09:38:40 -0400

    update

--- cloudGenomics.js
+++ cloudGenomics.js
@@ -11,8 +11,9 @@ document.body.appendChild(ip)
 ip.onkeyup=function(){console.log(ip.value)}
 s=document.createElement('script')
 s.src="https://code.jquery.com/jquery-2.1.4.min.js"
+
+console.log('jQuery ainda nao existe:',typeof(jQuery))
+document.head.appendChild(s)
 s.onload=function(){
     console.log('e agora ja existe:',typeof(jQuery)) // <-- prova que existes
 }
-console.log('jQuery ainda nao existe:',typeof(jQuery))
-document.head.appendChild(s)
